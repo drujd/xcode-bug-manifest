bd8205fc65f8b7442b24ab6bc20659b14d0b1a2b
Jan Klos <honza.klos@gmail.com>
2020-03-13 14:12:38 +0100

    Initial Commit

---
+++ Bug Test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Bug Test
+//
+//  Created by DDD on 13/03/2020.
+//  Copyright © 2020 FI MU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bug Test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Bug Test
+//
+//  Created by DDD on 13/03/2020.
+//  Copyright © 2020 FI MU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
